e321f17050bab8d65dd4039f4a8031ecd5a9f0a5
LJP1996 <769186324@qq.com>
2017-09-19 20:04:33 +0800

    递归题1

---
+++ recusion1/recursion1.py
@@ -0,0 +1,19 @@
+﻿def Counts(n,m):
+	print(n)
+	if(n>5000):
+		return Cmds(n/2,m)
+	else:
+		return Counts(n*2,m)
+
+def Cmds(n,m):
+	print(int(n))
+	if(n/2>0):
+		if(n==m):
+			return 0
+		return Cmds(n/2,m)
+
+def C():
+	x = int(input('请输入您需要计算的数:'))
+	Counts(x,x)
+	
+C()
